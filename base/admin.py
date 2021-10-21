b4f3a24a9a1a4f3492e8ea52ba99efd73048884e
Vladislava05 <vladislavac74@gmail.com>
2021-10-21 10:08:50 +0300

    Initial commit

---
+++ base/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import Task
+# Register your models here.
+admin.site.register(Task)
+
+
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
+
+
+ 
+ 
+
+
---
+++ base/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+
+# Reordering Form and View
+
+from django.forms import ModelForm
+from django.forms import Textarea
+
+
+
+        
+class PositionForm(forms.Form):
+    position = forms.CharField()
+
+
+
+
