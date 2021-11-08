60e57b48472c81fc8b1b912ba775daa65cbe065e
Vladislava05 <vladislavac74@gmail.com>
2021-11-08 21:27:42 +0300

    update

--- base/models.py
+++ base/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.utils import timezone
 
 
 
@@ -18,6 +18,8 @@ class Task(models.Model):
 
     def __str__(self):
         return self.title
+    
+    
    
     class Meta:
         ordering = ['complete']
