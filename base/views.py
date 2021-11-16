9f6ec3e9918ab9faf3afc632e0cf23c5379e73cb
Maxim Mukhin <stch.max.muhin@gmail.com>
2021-11-16 16:30:16 +0300

    feat: add top for Users by completed tasks

--- base/views.py
+++ base/views.py
@@ -1,19 +1,17 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
-
-
-from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.contrib.auth import login
+
+import operator
+
 from .models import Task
-
-
-
-
 
 
 class CustomLoginView(LoginView):
@@ -23,6 +21,7 @@ class CustomLoginView(LoginView):
 
     def get_success_url(self):
         return reverse_lazy('tasks')
+
 
 class RegisterPage(FormView):
     template_name = 'base/register.html'
@@ -37,34 +36,56 @@ class RegisterPage(FormView):
         return super(RegisterPage, self).form_valid(form)
         if User.objects.filter(username = request.POST['username']).exists():
               print('Already taken')
-            
-        
+
     def get(self, *args, **kwargs):
         if self.request.user.is_authenticated:
             return redirect('tasks')
         return super(RegisterPage, self).get(*args, **kwargs)
 
+
 class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = 'tasks'
+
+    def get_top_users(self):
+        users = User.objects.all()
+        top_users = {}
+
+        print(type(top_users))
+
+        for user in users:
+            tasks = user.tasks.all()
+            completed_tasks = 0
+            for task in tasks:
+                if task.complete:
+                    completed_tasks += 1
+            top_users.update({user: completed_tasks})
+
+        top_users = sorted(top_users.items(), key=lambda x:x[1], reverse=True)
+        top_users = dict(top_users)
+
+        return top_users
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['tasks'] = context['tasks'].filter(user=self.request.user)
         context['count'] = context['tasks'].filter(complete=False).count()
         context['k'] = context['tasks'].filter(complete=True).count()
-      
-        
+        context['top_users'] = self.get_top_users()
+
         search_input = self.request.GET.get('search-area') or ''
         if search_input:
             context['tasks'] = context['tasks'].filter(title__startswith=search_input)
         context['search_input'] = search_input
+
         return context
+
 
 class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'base/task.html'
+
 
 class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
@@ -75,17 +96,19 @@ class TaskCreate(LoginRequiredMixin, CreateView):
         form.instance.user = self.request.user
         return super(TaskCreate, self).form_valid(form)
 
+
 class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
     fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks')
+
 
 class DeleteView(LoginRequiredMixin, DeleteView):
     model = Task
     context_object_name = 'task'
     success_url = reverse_lazy('tasks')
 
+
 class About(ListView):
     model= Task
     template_name = 'base/about.html'
-
