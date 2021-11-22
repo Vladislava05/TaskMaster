b71c7877547f6672d9d419574ef2e9870c7b7a9c
Vladislava05 <vladislavac74@gmail.com>
2021-11-22 19:23:34 +0300

    Update settings.py

--- todo/settings.py
+++ todo/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'base.apps.BaseConfig',
+    
 ]
 
 MIDDLEWARE = [
@@ -53,6 +54,10 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'todo.urls'
+
+
+
+
 
 TEMPLATES = [
     {
