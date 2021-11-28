f745b134c9d6fa41c593db9d5490b54ffad8ebdf
Maxim Mukhin <stch.max.muhin@gmail.com>
2021-11-28 10:35:57 +0300

    chore: update admin panel

--- base/admin.py
+++ base/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.models import User
 
 from .models import Task
 
-admin.site.register(Task)
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'user', 'title', 'complete', 'created']
