1fbda677bfb8a1267a0c21d6a4244697a1e04503
Vladislava05 <vladislavac74@gmail.com>
2021-12-07 16:48:15 +0300

    update style

--- base/models.py
+++ base/models.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 class Task(models.Model):
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='tasks', null=True, blank=True)
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=35)
     description = models.TextField(null=True, blank=True)
     complete = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
